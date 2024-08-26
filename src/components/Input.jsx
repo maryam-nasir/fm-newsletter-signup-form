80d217083afb1e3d1337fa8392d33fc0c9821fb1
Maryam Nasir <maryamnasir555@yahoo.com>
2024-08-26 17:13:58 +0500

    Add Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,28 @@
+const Input = ({ name, type, label, placeholder, value, onChange, error }) => {
+  return (
+    <div className="flex flex-col mt-10 mb-5">
+      <div className="flex justify-between text-[10px] font-bold mb-2">
+        <label htmlFor={name} className="text-darkSlateGrey">
+          {label}
+        </label>
+        {error && <span className="text-tomato">{error}</span>}
+      </div>
+
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`h-12 border border-solid border-gray-300 rounded-md px-5 placeholder:text-gray-400 text-sm text-charcoalGrey focus:outline-0 focus:border-charcoalGrey ${
+          error
+            ? "text-tomato border-tomato focus:border-tomato bg-tomato/20"
+            : ""
+        }`}
+      />
+    </div>
+  );
+};
+
+export default Input;
