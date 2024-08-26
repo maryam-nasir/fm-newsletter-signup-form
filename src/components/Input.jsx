2faac830bf3240cce09d09bac4c5a82e9ee90625
Maryam Nasir <maryamnasir555@yahoo.com>
2024-08-26 17:23:15 +0500

    Modify onChange prop

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -14,7 +14,7 @@ const Input = ({ name, type, label, placeholder, value, onChange, error }) => {
         type={type}
         placeholder={placeholder}
         value={value}
-        onChange={onChange}
+        onChange={(event) => onChange(event.target.value)}
         className={`h-12 border border-solid border-gray-300 rounded-md px-5 placeholder:text-gray-400 text-sm text-charcoalGrey focus:outline-0 focus:border-charcoalGrey ${
           error
             ? "text-tomato border-tomato focus:border-tomato bg-tomato/20"
